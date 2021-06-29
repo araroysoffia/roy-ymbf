54dfd3a4a792ea88e301df0b23b9bf4d944333d4
royofficial <85897883+royofficial@users.noreply.github.com>
2021-06-29 11:21:34 +0700

    Create __cindy__.py

---
+++ lib/__cindy__.py
@@ -0,0 +1,20 @@
+pw_tambahan="""
+bismillah|sayang|anjing|kontol|indonesia
+""".strip().split("|")
+ngntd=((())>(()))+((())>(()))
+def awok(ngentod):
+	ngentod=ngentod.lower()
+	return [ngentod,ngentod+"123",ngentod+"1234",ngentod+"12345"]
+def pw_list(ngentot):
+	if len(ngentot)==2:
+		_yayanganteng_=ngentot[1].split(" ")
+		if len(_yayanganteng_[ngntd])!=ngntd:
+			asu=awok(_yayanganteng_[ngntd])
+			if len(_yayanganteng_[ngntd])<=5:
+				del asu[ngntd]
+				for x in pw_tambahan: asu.append(x)
+			else:
+				for x in pw_tambahan: asu.append(x)
+		else: asu=pw_tambahan
+	else: asu=pw_tambahan
+	return asu
