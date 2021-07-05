19a57a57c5e3d0c27252fffbab3f166f3af5e7d1
royofficial <85897883+royofficial@users.noreply.github.com>
2021-07-05 23:38:54 +0700

    Update __cindy__.py

--- lib/__cindy__.py
+++ lib/__cindy__.py
@@ -3,18 +3,17 @@ bismillah|sayang
 """.strip().split("|")
 ngntd=((())>(()))+((())>(()))
 def awok(ngentod):
-	ngentod=ngentod.lower()
 	return [ngentod,ngentod+"123",ngentod+"1234",ngentod+"12345"]
 def pw_list(ngentot):
 	if len(ngentot)==2:
 		_yayanganteng_=ngentot[1].split(" ")
 		if len(_yayanganteng_[ngntd])!=ngntd:
-			asu=awok(_yayanganteng_[ngntd])
-			if len(_yayanganteng_[ngntd])<=5:
+			asu=awok(_yayanganteng_[ngntd].lower())
+			if len(_yayanganteng_[ngntd]) < 6:
 				del asu[ngntd]
-				for x in pw_tambahan: asu.append(x)
+				[asu.append(x) for x in pw_tambahan]
 			else:
-				for x in pw_tambahan: asu.append(x)
+				[asu.append(x) for x in pw_tambahan]
 		else: asu=pw_tambahan
 	else: asu=pw_tambahan
 	return asu
